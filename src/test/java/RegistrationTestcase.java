21374ab2688abe529bc0ba5ac37c780c3c2b0380
HrishabhKushwah <hrishabhkushwah@hotmail.com>
2024-05-16 23:54:52 +0530

    Modification

--- src/test/java/RegistrationTestcase.java
+++ src/test/java/RegistrationTestcase.java
@@ -1,5 +1,6 @@
 public class RegistrationTestcase {
     public static void main(String[] args) {
         System.out.println("Registration testcase");
+        System.out.println("Registration testcase1");
     }
 }
